33601a632325fd25989230262d13f509c5dfa8ff
Akash Kumar Shaw <58072860+akashxlr8@users.noreply.github.com>
2025-11-25 23:14:59 +0530

    refactor: Remove unused AgentState class and related imports from agent.py

--- src/agent.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict, Annotated, Sequence
-from langchain_core.messages import BaseMessage
-
-
-class AgentState(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], "The messages in the conversation"]
-    user_id: str
-    thread_name: str
