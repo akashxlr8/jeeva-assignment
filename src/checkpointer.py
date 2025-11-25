c5745793a6716cddd80148cb5b204158caaa41d7
Akash Kumar Shaw <58072860+akashxlr8@users.noreply.github.com>
2025-11-25 22:20:54 +0530

    refactor: Remove checkpointer module as it is no longer needed

--- src/checkpointer.py
+++
@@ -1,5 +0,0 @@
-from langgraph.checkpoint.sqlite import SqliteSaver
-
-def get_checkpointer(db_path: str = "chat.db"):
-    """Get a SQLite checkpointer for persistent storage."""
-    return SqliteSaver.from_conn_string(db_path)
